d80a271fc71fa3604a4adbba93e72ffcae796e0a
Ayush Soni <soni.ayush.2212@gmail.com>
2024-12-30 20:26:54 +0530

    Add bouncing ball

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,58 @@
 #include <SFML/Graphics.hpp>
 
+const int SCREEN_WIDTH = 800;
+const int SCREEN_HEIGHT = 800;
+
+void checkBounds(sf::CircleShape &ball, sf::RenderWindow &window, sf::Vector2f velocity)
+{
+    // Collision detection with window boundaries
+    if (ball.getPosition().x + ball.getRadius() * 2 > window.getSize().x || ball.getPosition().x < 0)
+    {
+        velocity.x = -velocity.x; // Reverse x velocity
+    }
+    if (ball.getPosition().y + ball.getRadius() * 2 > window.getSize().y || ball.getPosition().y < 0)
+    {
+        velocity.y = -velocity.y; // Reverse y velocity
+    }
+}
+
 int main()
 {
-    auto window = sf::RenderWindow(sf::VideoMode({1920u, 1080u}), "CMake SFML Project");
-    window.setFramerateLimit(144);
+    sf::RenderWindow window(sf::VideoMode({SCREEN_WIDTH, SCREEN_HEIGHT}), "My window");
+
+    // Ball propertiesk
+    sf::CircleShape ball(20.f);
+    ball.setFillColor(sf::Color::Red);
+    ball.setPosition(sf::Vector2f{SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2});
+    sf::Vector2f velocity(0, 0); // Initial velocity
+    float gravity = 0.001;
 
     while (window.isOpen())
     {
         while (const std::optional event = window.pollEvent())
         {
             if (event->is<sf::Event::Closed>())
-            {
                 window.close();
-            }
+        }
+        // Update
+
+        // Collision detection with window boundaries
+        if (ball.getPosition().x + ball.getRadius() * 2 > window.getSize().x || ball.getPosition().x < 0)
+        {
+            velocity.x = -velocity.x; // Reverse x velocity
+        }
+        if (ball.getPosition().y + ball.getRadius() * 2 > window.getSize().y || ball.getPosition().y < 0)
+        {
+            velocity.y = -velocity.y;// Reverse y velocity
         }
 
-        window.clear();
+        velocity.y += gravity;
+        ball.move(velocity);
+
+        window.clear(sf::Color::Black);
+
+        window.draw(ball);
+
         window.display();
     }
 }
